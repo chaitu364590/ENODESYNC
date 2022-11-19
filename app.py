fea43a2a6932d507d99df8a11109122ce26d45b3
chaitu364590 <84063946+chaitu364590@users.noreply.github.com>
2022-11-19 11:37:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ st.caption("AR FOR IOT")
 st.markdown("---")
 
 ur = "https://my.spline.design/3dtextbluecopy-86cab89ddbb0c385dbd3942a6b187088/"
+if st.button("AR_",key="2"):
+    st.video("3.mp4")
 
-st.button("AR",key="2")
 st.write("[AR](%s)" % ur)
 u = "https://my.spline.design/purpleiconsset1copy-6c433258002d4b52f5860fcdd49b4aeb/"
-st.button("AR_",key="3")
 st.write("[AR](%s)" % u)
 
 l = "https://my.spline.design/keyboardrecordercopy-41ae85a220ef685a15dc6e8a59ea3596/"
-st.button("AR_",key="4")
+    
 st.write("[AR](%s)" % l)    
